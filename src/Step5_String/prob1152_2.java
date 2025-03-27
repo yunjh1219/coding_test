51ea9377ace6261312b1e7001b49a9abf23a2efa
yunjh1219 <orenjh1219@gmail.com>
2025-03-27 09:51:22 +0900

    feat:BOJ 1152번: 단어의 개수 BufferedReadder, StringTokenizer 사용

--- src/Step5_String/prob1152_2.java
+++ src/Step5_String/prob1152_2.java
@@ -4,12 +4,16 @@ import java.util.StringTokenizer;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+/*
+ BufferedRader 로 문자열 입력받고,
+ 입력받은 문자열을 StringTokenizer 사용하여 " " 구분하여 저장
+ countTokens() 로 갯수 반환
+ */
 
 public class prob1152_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        System.out.print(st.countTokens());
     }
 }
