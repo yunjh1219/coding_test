a330965850df43b4be763270d9089556c22b63a8
yunjh1219 <orenjh1219@gmail.com>
2025-03-30 08:47:57 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,30 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[]args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-	  int stu[] = new int[30];
-	  int sto[] = new int[30];
-	  
-	  for (int i=0; i<stu.length; i++) {
-		  stu[i] = i+1;
-	  }
-	  
-	  for (int i=0; i<28; i++) {
-		  int n = sc.nextInt();
-		  sto[n-1] = n;
-	  }
-	  
-        
-        for( int i=0; i<30; i++) {
-        	if(sto[i] != stu[i]) {
-        	 
-        		System.out.println(stu[i]);
-        	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input[] = new int[30];//제출 학생
+        int att[] = new int[30];  //총 학생
+        boolean stu = false;
+
+        for(int i=0; i<30; i++){   //총 학생에 값 넣기
+            att[i] = i+1;          //i+1 → [0] 1번 부터 [29] 30번까지
         }
-        sc.close();
-	}
+
+        for(int i=0; i<28; i++){
+            int N = Integer.parseInt(br.readLine());
+            input[N-1] = N;        //제출 학생 입력 
+        }
+        br.close();
+        
+        for(int i=0; i<30; i++) {
+            if (input[i] != att[i]) {
+                System.out.println(att[i]);
+            }
+        }
+    }
 }
