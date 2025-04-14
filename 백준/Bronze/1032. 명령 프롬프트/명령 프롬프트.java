b56aae18f2f433bc90dd050e5d065d4516f40004
yunjh1219 <orenjh1219@gmail.com>
2025-04-14 14:06:26 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,21 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
         char[] pattern = br.readLine().toCharArray();
-
-        for (int i = 0; i < N - 1; i++) {
-            char[] fileName = br.readLine().toCharArray();
-
-            for (int j = 0; j < pattern.length; j++) {
-                if (pattern[j] != fileName[j]) pattern[j] = '?';
+        
+        for(int i=0; i<N-1; i++){
+            char[] Input = br.readLine().toCharArray();
+            
+            for(int j=0; j<pattern.length; j++){
+                if(pattern[j] != Input[j]){
+                    pattern[j] = '?';
+                }
             }
         }
-
-        System.out.println(pattern);
+     System.out.println(new String(pattern));  
     }
 }
