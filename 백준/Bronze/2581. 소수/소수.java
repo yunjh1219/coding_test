06ecba6bbeb4c1683db6a07c4638632cb5a75099
yunjh1219 <orenjh1219@gmail.com>
2025-05-08 14:26:43 +0900

    [Bronze II] Title: 소수, Time: 108 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+
+    public static boolean isPrime(int a){
+        if(a < 2) return false;
+        for(int i=2; i<=Math.sqrt(a); i++){
+            if(a%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = 10000;
+
+        for(int i=M; i<=N; i++){
+            if(isPrime(i)){
+                sum += i;
+                if(i < min){
+                    min = i ;
+                }
+            }
+        }
+
+        if (sum == 0){
+            System.out.print(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
