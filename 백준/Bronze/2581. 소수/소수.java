3322e702feb58d178877f87eb6bf1afe21f0f16c
yunjh1219 <orenjh1219@gmail.com>
2025-05-08 15:33:19 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static boolean isPrime(int a){
         if(a < 2) return false;
-        for(int i=2; i<=Math.sqrt(a); i++){
+        for(int i=2; i*i<=a; i++){
             if(a%i == 0){
                 return false;
             }
