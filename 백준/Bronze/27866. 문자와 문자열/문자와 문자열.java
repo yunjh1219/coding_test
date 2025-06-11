a59bdf2535a8403c8f84cd60d98c2ca7b6c6673f
yunjh1219 <orenjh1219@gmail.com>
2025-06-12 01:37:46 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,16 +1,15 @@
-//문자와 문자열
-//charAt() 메서드를 알아야 함.
-//s의 i번째 글자를 출력한다.
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[]args) {
-	  Scanner sc = new Scanner(System.in);
-	  
-	  String s = sc.nextLine(); //ex. Sprout 의 3번째는 r
-	  int i = sc.nextInt()-1;   /* index는 0부터 시작. 
-	                               그렇기에 index-1 해줘야 함.*/
-	  System.out.print(s.charAt(i));
-	  
-	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        char []cr = str.toCharArray();
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.print(cr[N-1]);
+
+    }
 }
