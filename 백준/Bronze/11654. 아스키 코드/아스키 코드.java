88c7977504855841ee89574dfca89b41324fe967
yunjh1219 <orenjh1219@gmail.com>
2025-06-13 22:37:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+        System.out.print((int)ch);
+    }
+}
