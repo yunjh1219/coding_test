35dc7e97a1a3281de15a789cf64e7056a55a5fd4
yunjh1219 <orenjh1219@gmail.com>
2025-08-17 22:55:37 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";   // 짝수일 때
+        } else {
+            return "Odd";    // 홀수일 때
+        }
+    }
+}
